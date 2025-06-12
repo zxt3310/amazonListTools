14028de84bb4530c856bcf2736d2c6637648f477
zxt3310 <zxt3310@163.com>
2025-06-12 18:42:28 +0800

    update notification

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,80 @@
 <script>
 	import Pusher from 'pusher-js';
 	export default {
+		data() {
+			return {
+				//浏览器通知
+				is_allow_notify: false,
+				//系统权限
+				sys_notify_allow: false
+			}
+		},
 		mounted() {
-			// let pusher = new Pusher('90d548917a736f8c6e77', {
-			//       cluster: 'mt1'
-			//     });
-			
-			//     let channel = pusher.subscribe('public-notification-new');
-			//     channel.bind('new-notification', (e) => {
-			//       console.log(e)
-			//     });
-			// console.log(this.$echo)
+			//请求通知权限
+			this.requestNotificationPermission()
 			this.$echo.channel('public-notification')
-				.listen('.new-notification', (e) => { // 注意事件名前加点号
-					// console.log("接收到通知")
-					console.log(e)
-					// this.$alert(e.message)
+				.listen('.new-notification', (e) => { // 注意事件名前加点号		
+					this.showNotification(e.message, e.type,e.path)
 				});
+		},
+		methods: {
+			requestNotificationPermission() {
+				// 检查是否支持通知
+				if (!("Notification" in window)) {
+					console.log("您的浏览器不支持系统通知")
+					alert("您的浏览器不支持系统通知");
+					return;
+				}
+
+				if (Notification.permission === "granted") {
+					this.is_allow_notify = true;
+				} else if (Notification.permission !== "denied") {
+					// 否则请求权限
+					Notification.requestPermission().then(permission => {
+						if (permission === "granted") {
+							this.is_allow_notify = true;
+						} else if (permission === 'denied')
+							this.is_allow_notify = false;
+					});
+				} else
+					this.is_allow_notify = false;
+				
+				
+				this.showTestNotification();
+			},
+
+			showNotification(message, type, path) {
+				const notification = new Notification(type, {
+					body: message,
+					requireInteraction: false // 需要用户手动关闭
+				});
+				notification.onclick = (event) => {
+					event.preventDefault()
+					window.focus();
+					// 点击通知时跳转到页面
+					window.location.href = path;
+					notification.close()
+				};
+				notification.onclose = (event)=>{
+					console.log("通知被用户手动关闭")
+				}
+				// 成功通知
+				if(this.is_allow_notify == false){
+					this.$toast.success(message, type);
+				}
+			},
+			showTestNotification(){
+				const notification = new Notification('测试通知', {
+					body: '这是一条测试通知，请手动关闭或点击通知完成验证',
+					requireInteraction: true // 需要用户手动关闭
+				});
+				notification.onclick = ()=>{
+					this.is_allow_notify = true
+				}
+				notification.onclose = () =>{
+					this.is_allow_notify = true
+				}
+			}
 		}
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,17 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import MyNavi from "./components/Navi.vue";
 import EchoInstance from "./js/bootstrap.js";
+import VueIziToast from 'vue-izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+// 默认配置（可选）
+const options = {
+  position: 'topRight',
+  timeout: 5000,
+  progressBar: true,
+  transitionIn: 'fadeInUp',
+};
+Vue.use(VueIziToast, options);
 
 Vue.prototype.$echo = EchoInstance;
 Vue.config.silent = true; // 关闭所有Vue日志和警告（生产环境默认启用）
